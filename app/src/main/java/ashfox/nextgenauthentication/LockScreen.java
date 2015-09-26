f46a7914163b887e3a4520abcc7fdba657969a91
Anirudh Ramanathan <anirudh4444@gmail.com>
2015-09-26 15:21:09 -0500

    Modifications to event handling and responses

--- app/src/main/java/ashfox/nextgenauthentication/LockScreen.java
+++ app/src/main/java/ashfox/nextgenauthentication/LockScreen.java
@@ -10,7 +10,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.widget.GridView;
+import android.widget.ListAdapter;
 import android.widget.Toast;
+
+import ashfox.nextgenauthentication.util.ImageAdapter;
 
 /**
 
@@ -23,6 +27,13 @@ public class LockScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_screen);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
+        final ListAdapter adp = new ImageAdapter(this);
+        gridview.setAdapter(adp);
+
+
     }
 
 }
